bee883ef5a8010c26a14f7e4157d2b584c788397
Lukas Pranciliauskas <l.pranciliauskas@gmail.com>
2018-06-20 12:04:42 -0500

    finished chapter 8

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#help'
   get '/contact', to: 'static_pages#contact'
   get '/about', to: 'static_pages#about'
+  post '/signup',  to: 'users#create'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
